5183e2d8f89657260fd01743607a7e6154b75cce
Aaron Topping <aaronjtopping@gmail.com>
2024-12-15 18:12:16 -0700

    Updating permissions

--- project/scripts/env.js
+++ project/scripts/env.js
@@ -1,6 +1,8 @@
-// Using Github Secrets I am able to secretly store my keys
+// Let's obfuscate my key a little... for the public publishing for school project
+const k = ['b42bc', '0816a', '2a469293f1', '766e14cd26fd']
+const c = ['ObyQXnT', 'A7e1mWg', 'i8ntFEevO', '21nvEKxp3']
 
 const ENV = {
-    CLIENT_ID: '{{ CLIENT_ID }}',
-    CLIENT_SECRET: '{{ CLIENT_SECRET }}'
+    CLIENT_ID: `${k[0]}${k[1]}${k[2]}${k[3]}`,
+    CLIENT_SECRET: `${c[0]}${c[1]}${c[2]}${c[3]}`
 };
